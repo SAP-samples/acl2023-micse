ccddb84b73107709e589a709fb8714ea07fda4f8
Tassilo Klein <TJKlein@gmail.com>
2023-08-17 13:10:07 +0200

    Add files via upload

---
+++ model/models.py
@@ -0,0 +1,525 @@
+#
+# SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+import logging
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.distributed as dist
+import einops
+import wandb
+import numpy as np
+import transformers
+from transformers import RobertaTokenizer
+from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel, RobertaModel, RobertaLMHead
+from transformers.models.bert.modeling_bert import BertPreTrainedModel, BertModel, BertLMPredictionHead
+from transformers.activations import gelu
+from transformers.file_utils import (
+    add_code_sample_docstrings,
+    add_start_docstrings,
+    add_start_docstrings_to_model_forward,
+    replace_return_docstrings,
+)
+from transformers.modeling_outputs import SequenceClassifierOutput, BaseModelOutputWithPoolingAndCrossAttentions
+logger = logging.getLogger(__name__)
+
+def format_attention(attention):
+    squeezed = []
+    for layer_attention in attention:
+        # 1 x num_heads x seq_len x seq_len
+        if len(layer_attention.shape) != 4:
+            raise ValueError("The attention tensor does not have the correct number of dimensions. Make sure you set "
+                             "output_attentions=True when initializing your model.")
+        squeezed.append(layer_attention.squeeze(0))
+    # num_layers x num_heads x seq_len x seq_len
+    if attention[0].shape[0] > 1:
+        return torch.stack(squeezed).permute(1, 0, 2, 3, 4)
+    else:
+        return torch.stack(squeezed).view(1, torch.stack(squeezed).shape[0], torch.stack(squeezed).shape[1], torch.stack(squeezed).shape[2], torch.stack(squeezed).shape[3])
+
+
+class MLPLayer(nn.Module):
+    """
+    Head for getting sentence representations over RoBERTa/BERT's CLS representation.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, features, **kwargs):
+        x = self.dense(features)
+        x = self.activation(x)
+
+        return x
+
+class Similarity(nn.Module):
+    """
+    Dot product or cosine similarity
+    """
+
+    def __init__(self, temp):
+        super().__init__()
+        self.temp = temp
+        self.cos = nn.CosineSimilarity(dim=-1)
+
+    def forward(self, x, y):
+        return self.cos(x, y) / self.temp
+
+
+class Pooler(nn.Module):
+    """
+    Parameter-free poolers to get the sentence embedding
+    'cls': [CLS] representation with BERT/RoBERTa's MLP pooler.
+    'cls_before_pooler': [CLS] representation without the original MLP pooler.
+    'avg': average of the last layers' hidden states at each token.
+    'avg_top2': average of the last two layers.
+    'avg_first_last': average of the first and the last layers.
+    """
+    def __init__(self, pooler_type):
+        super().__init__()
+        self.pooler_type = pooler_type
+        assert self.pooler_type in ["cls", "cls_before_pooler", "avg", "avg_top2", "avg_first_last"], "unrecognized pooling type %s" % self.pooler_type
+
+    def forward(self, attention_mask, outputs):
+        last_hidden = outputs.last_hidden_state
+        pooler_output = outputs.pooler_output
+        hidden_states = outputs.hidden_states
+
+        if self.pooler_type in ['cls_before_pooler', 'cls']:
+            return last_hidden[:, 0]
+        elif self.pooler_type == "avg":
+            return ((last_hidden * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1))
+        elif self.pooler_type == "avg_first_last":
+            first_hidden = hidden_states[0]
+            last_hidden = hidden_states[-1]
+            pooled_result = ((first_hidden + last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+            return pooled_result
+        elif self.pooler_type == "avg_top2":
+            second_last_hidden = hidden_states[-2]
+            last_hidden = hidden_states[-1]
+            pooled_result = ((last_hidden + second_last_hidden) / 2.0 * attention_mask.unsqueeze(-1)).sum(1) / attention_mask.sum(-1).unsqueeze(-1)
+            return pooled_result
+        else:
+            raise NotImplementedError
+
+
+def cl_init(cls, config):
+    """
+    Contrastive learning class init function.
+    """
+    cls.pooler_type = cls.model_args.pooler_type
+    cls.pooler = Pooler(cls.model_args.pooler_type)
+    if cls.model_args.pooler_type == "cls":
+        cls.mlp = MLPLayer(config)
+    cls.sim = Similarity(temp=cls.model_args.temp)
+    cls.init_weights()
+
+def cl_forward(cls,
+    encoder,
+    input_ids=None,
+    attention_mask=None,
+    token_type_ids=None,
+    position_ids=None,
+    head_mask=None,
+    inputs_embeds=None,
+    labels=None,
+    output_attentions=None,
+    output_hidden_states=None,
+    return_dict=None,
+    mlm_input_ids=None,
+    mlm_labels=None,
+):
+    return_dict = return_dict if return_dict is not None else cls.config.use_return_dict
+    ori_input_ids = input_ids
+    batch_size = input_ids.size(0)
+    # Number of sentences in one instance
+    # 2: pair instance; 3: pair instance with a hard negative
+    num_sent = input_ids.size(1)
+
+    mlm_outputs = None
+    # Flatten input for encoding
+    input_ids = input_ids.view((-1, input_ids.size(-1))) # (bs * num_sent, len)
+    attention_mask = attention_mask.view((-1, attention_mask.size(-1))) # (bs * num_sent len)
+    if token_type_ids is not None:
+        token_type_ids = token_type_ids.view((-1, token_type_ids.size(-1))) # (bs * num_sent, len)
+
+    # Get raw embeddings
+    outputs = encoder(
+        input_ids,
+        attention_mask=attention_mask,
+        token_type_ids=token_type_ids,
+        position_ids=position_ids,
+        head_mask=head_mask,
+        inputs_embeds=inputs_embeds,
+        output_attentions=True,
+        output_hidden_states=True if cls.model_args.pooler_type in ['avg_top2', 'avg_first_last'] else False,
+        return_dict=True,
+    )
+
+    if cls.model_args.style == 'miCSE':
+        attn = format_attention(outputs['attentions'])
+
+        # sub-selection of attention and chunking
+        
+        # layers
+        attn = attn[:, np.min([cls.model_args.min_layer, cls.model_args.max_layer]):np.max(
+            [cls.model_args.min_layer, cls.model_args.max_layer]), :, :, :]
+        mlm_attn = einops.rearrange(
+            attn, "(h i) (j m ) k t z-> (h k m) i (j t z)",m=2,i=2)
+
+
+    # MLM auxiliary objective
+    if mlm_input_ids is not None:
+        mlm_input_ids = mlm_input_ids.view((-1, mlm_input_ids.size(-1)))
+        mlm_outputs = encoder(
+            mlm_input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=True if cls.model_args.pooler_type in ['avg_top2', 'avg_first_last'] else False,
+            return_dict=True,
+        )
+
+    # Pooling
+    pooler_output = cls.pooler(attention_mask, outputs)
+    pooler_output = pooler_output.view((batch_size, num_sent, pooler_output.size(-1))) # (bs, num_sent, hidden)
+
+    # If using "cls", we add an extra MLP layer
+    # (same as BERT's original implementation) over the representation.
+    if cls.pooler_type == "cls":
+        pooler_output = cls.mlp(pooler_output)
+
+    # Separate representation
+    z1, z2 = pooler_output[:,0], pooler_output[:,1]
+
+    # Hard negative
+    if num_sent == 3:
+        z3 = pooler_output[:, 2]
+
+    # Gather all embeddings if using distributed training
+    if dist.is_initialized() and cls.training:
+        # Gather hard negative
+        if num_sent >= 3:
+            z3_list = [torch.zeros_like(z3) for _ in range(dist.get_world_size())]
+            dist.all_gather(tensor_list=z3_list, tensor=z3.contiguous())
+            z3_list[dist.get_rank()] = z3
+            z3 = torch.cat(z3_list, 0)
+
+        # Dummy vectors for allgather
+        z1_list = [torch.zeros_like(z1) for _ in range(dist.get_world_size())]
+        z2_list = [torch.zeros_like(z2) for _ in range(dist.get_world_size())]
+        # Allgather
+        dist.all_gather(tensor_list=z1_list, tensor=z1.contiguous())
+        dist.all_gather(tensor_list=z2_list, tensor=z2.contiguous())
+
+        # Since allgather results do not have gradients, we replace the
+        # current process's corresponding embeddings with original tensors
+        z1_list[dist.get_rank()] = z1
+        z2_list[dist.get_rank()] = z2
+        # Get full batch embeddings: (bs x N, hidden)
+        z1 = torch.cat(z1_list, 0)
+        z2 = torch.cat(z2_list, 0)
+
+
+    if cls.model_args.style == 'miCSE':
+      
+
+        if True: #cls.counter >= cls.config.mi_startstep:
+          
+            # extract two views (X=view1, Y=view2)
+            X2 = (mlm_attn[:, 0]) 
+            Y2 = (mlm_attn[:, 1]) 
+
+            # number of random samples
+            target_samples = 150
+            # dummy loss value in case of NaN
+            DUMMY_VALUE = 100
+            
+        
+
+            mask_XY = torch.logical_and(X2 > 0, Y2 > 0).type(
+                torch.DoubleTensor).to(cls.device)
+            
+            nonzero_ratio = -mask_XY.sum()/mask_XY.numel()
+               
+            # find non-zero entries
+            idx = (mask_XY.sum(dim=1) >=  target_samples).nonzero()
+
+            #if there are no data points with sufficient samples, reduce number of samples
+            if idx.shape[0] < 1:
+                
+                # trying to reduce the number of samples
+                samples = int(np.min(mask_XY.sum(dim=1).detach().cpu().numpy().tolist() +[target_samples]))
+                
+                ratio = samples/target_samples
+                
+                if ratio < 0.5:
+                    logger.warning("No data points with "+str(target_samples)+ " samples.")
+            else:
+                if idx.shape[0] < mask_XY.shape[0]:
+                    ratio = idx.shape[0]/mask_XY.shape[0]
+                    
+                    if ratio < 0.5:
+                        logger.warning("Only "+str(idx.shape[0])+" out of "+str(mask_XY.shape[0])+" samples taken.")
+                samples = target_samples
+                mask_XY = mask_XY[idx].squeeze(1)
+                X2 = X2[idx].squeeze(1)
+                Y2 = Y2[idx].squeeze(1)
+                
+                
+            
+            
+            if samples > 0:
+            
+                select_XY = torch.multinomial(mask_XY, samples).to(cls.device)
+                idx_XY = torch.zeros(
+                    X2.shape[0], X2.shape[1], device=cls.device).scatter_(1, select_XY, 1)
+
+                X3 = X2[idx_XY > 0].view(X2.shape[0], samples)
+                Y3 = Y2[idx_XY > 0].view(Y2.shape[0], samples)
+                # convert from log-normal distributed variables to normal distributed
+                X3 = torch.log(X3)
+                Y3 = torch.log(Y3)
+
+                labels = torch.arange(X2.size(0)).long().to(cls.device)
+                loss_fct = nn.CrossEntropyLoss()
+
+
+                cos = nn.CosineSimilarity(dim=-1, eps=1e-6)
+
+                # compute correlation coefficient
+                x2_statistic = X3 - X3.mean(dim=1, keepdim=True)
+                y2_statistic = Y3 - Y3.mean(dim=1, keepdim=True)
+                cos_sim = cos(x2_statistic.unsqueeze(1),
+                                y2_statistic.unsqueeze(0))
+                
+                cos_sim = torch.diagonal(cos_sim)
+                eps=1e-6
+                # scale up similarity (optional)
+                cos_sim = -0.5 * torch.log(1.00-torch.pow(cos_sim, 2.0))
+                
+                scale = (mask_XY.sum()/mask_XY.numel())**2.0+eps
+                MI_attn = -torch.mean(cos_sim)*scale
+                # Check if MI is NaN, set to high value
+                if MI_attn != MI_attn:
+                    MI_attn = torch.tensor(DUMMY_VALUE).cuda()
+            else:
+                MI_attn = torch.tensor(0).cuda()
+                
+
+    if cls.model_args.style == 'SimCSE' or cls.model_args.style == 'miCSE':
+
+        cos_sim = cls.sim(z1.unsqueeze(1), z2.unsqueeze(0))
+        if cls.model_args.diag_only:
+            cos_sim = cos_sim - torch.triu(cos_sim, diagonal=1) - torch.tril(cos_sim, diagonal=-1)
+            
+        # Hard negative
+        if num_sent >= 3:
+            z1_z3_cos = cls.sim(z1.unsqueeze(1), z3.unsqueeze(0))
+            cos_sim = torch.cat([cos_sim, z1_z3_cos], 1)
+
+        labels = torch.arange(cos_sim.size(0)).long().to(cls.device)
+        loss_fct = nn.CrossEntropyLoss()
+
+        # Calculate loss with hard negatives
+        if num_sent == 3:
+            # Note that weights are actually logits of weights
+            z3_weight = cls.model_args.hard_negative_weight
+            weights = torch.tensor(
+                [[0.0] * (cos_sim.size(-1) - z1_z3_cos.size(-1)) + [0.0] * i + [z3_weight] + [0.0] * (z1_z3_cos.size(-1) - i - 1) for i in range(z1_z3_cos.size(-1))]
+            ).to(cls.device)
+            cos_sim = cos_sim + weights
+
+        loss = loss_fct(cos_sim, labels)
+        wandb.log({"SimCSE_loss": loss.item()})
+    if cls.model_args.style == 'miCSE':
+
+        loss = cls.config.task_alpha * loss + (MI_attn) * cls.config.task_lambda #+ nonzero_ratio * cls.config.task_gamma
+
+        if torch.is_tensor(MI_attn):
+            wandb.log({"MI_normal": MI_attn.item()}) # "Nonzero_ratio": nonzero_ratio,
+
+    # Calculate loss for MLM
+    if mlm_outputs is not None and mlm_labels is not None:
+        mlm_labels = mlm_labels.view(-1, mlm_labels.size(-1))
+        prediction_scores = cls.lm_head(mlm_outputs.last_hidden_state)
+        masked_lm_loss = loss_fct(prediction_scores.view(-1, cls.config.vocab_size), mlm_labels.view(-1))
+        wandb.log({"masked_lm_loss": masked_lm_loss.item()})
+        loss = loss + cls.model_args.mlm_weight * masked_lm_loss
+    wandb.log({"loss": loss.item()})
+    if not return_dict:
+        output = (cos_sim,) + outputs[2:]
+        return ((loss,) + output) if loss is not None else output
+    return SequenceClassifierOutput(
+        loss=loss,
+        logits=cos_sim,
+        hidden_states=outputs.hidden_states,
+        attentions=outputs.attentions,
+    )
+
+
+def sentemb_forward(
+    cls,
+    encoder,
+    input_ids=None,
+    attention_mask=None,
+    token_type_ids=None,
+    position_ids=None,
+    head_mask=None,
+    inputs_embeds=None,
+    labels=None,
+    output_attentions=None,
+    output_hidden_states=None,
+    return_dict=None,
+):
+
+    return_dict = return_dict if return_dict is not None else cls.config.use_return_dict
+
+    outputs = encoder(
+        input_ids,
+        attention_mask=attention_mask,
+        token_type_ids=token_type_ids,
+        position_ids=position_ids,
+        head_mask=head_mask,
+        inputs_embeds=inputs_embeds,
+        output_attentions=output_attentions,
+        output_hidden_states=True if cls.pooler_type in ['avg_top2', 'avg_first_last'] else False,
+        return_dict=True,
+    )
+
+    pooler_output = cls.pooler(attention_mask, outputs)
+    if cls.pooler_type == "cls" and not cls.model_args.mlp_only_train:
+        pooler_output = cls.mlp(pooler_output)
+
+    if not return_dict:
+        return (outputs[0], pooler_output) + outputs[2:]
+
+    return BaseModelOutputWithPoolingAndCrossAttentions(
+        pooler_output=pooler_output,
+        last_hidden_state=outputs.last_hidden_state,
+        hidden_states=outputs.hidden_states,
+    )
+
+
+class BertForCL(BertPreTrainedModel):
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+
+    def __init__(self, config, *model_args, **model_kargs):
+        super().__init__(config)
+        self.model_args = model_kargs["model_args"]
+        self.bert = BertModel(config, add_pooling_layer=False)
+
+        if self.model_args.do_mlm:
+            self.lm_head = BertLMPredictionHead(config)
+
+        cl_init(self, config)
+
+    def forward(self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        sent_emb=False,
+        mlm_input_ids=None,
+        mlm_labels=None,
+    ):
+        if sent_emb:
+            return sentemb_forward(self, self.bert,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+        else:
+            return cl_forward(self, self.bert,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                mlm_input_ids=mlm_input_ids,
+                mlm_labels=mlm_labels,
+            )
+
+
+
+class RobertaForCL(RobertaPreTrainedModel):
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+
+    def __init__(self, config, *model_args, **model_kargs):
+        super().__init__(config)
+        self.model_args = model_kargs["model_args"]
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+
+        if self.model_args.do_mlm:
+            self.lm_head = RobertaLMHead(config)
+
+        cl_init(self, config)
+
+    def forward(self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        sent_emb=False,
+        mlm_input_ids=None,
+        mlm_labels=None,
+    ):
+        if sent_emb:
+            return sentemb_forward(self, self.roberta,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+        else:
+            return cl_forward(self, self.roberta,
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                mlm_input_ids=mlm_input_ids,
+                mlm_labels=mlm_labels,
+            )
