92a9cc804652b35e42295af97e3da7804742a91d
Tassilo Klein <TJKlein@gmail.com>
2023-08-18 11:24:34 +0200

    Update download_dataset.sh

--- SentEval/data/downstream/download_dataset.sh
+++ SentEval/data/downstream/download_dataset.sh
@@ -1,7 +1,2 @@
-#
-# SPDX-FileCopyrightText: 2017 Facebook, Inc.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
 wget https://huggingface.co/datasets/princeton-nlp/datasets-for-simcse/resolve/main/senteval.tar
 tar xvf senteval.tar
